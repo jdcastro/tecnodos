450eef315567a98c9bdbafc0b2663fa05e48e03e
Johnny De Castro M. <j@jdcastro.co>
2025-08-21 08:22:21 -0500

    integración visualización en modulo agrovista

--- project/app/modules/agrovista/web_routes.py
+++ project/app/modules/agrovista/web_routes.py
@@ -1,7 +1,26 @@
-from flask import render_template
+from flask import render_template, url_for
 from . import agrovista as web
 
+def get_dashboard_menu():
+    """Define el menu superior en los templates"""
+    return {
+        "menu": [
+            {"name": "Home", "url": url_for("core.index")},
+            {"name": "Logout", "url": url_for("core.logout")},
+            {"name": "Profile", "url": url_for("core.profile")},
+        ]
+    }
 
 @web.route("/", methods=["GET"])
 def hello():
-    return render_template("agrovista/index.j2")
+    context = {
+    "dashboard": True,
+    "title": "NDVI Tool",
+    "description": "Herramienta para el analisis NDVI.",
+    "author": "Johnny De Castro",
+    "site_title": "Análisis de Imagenes",
+    "data_menu": get_dashboard_menu(),
+    "entity_name": "Reportes",
+    "entity_name_lower": "reporte",
+ }
+    return render_template("agrovista/ndvi-tool.j2", **context)
